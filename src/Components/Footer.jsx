b579419a27128880ab8d09004e6dd363e2d995ae
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2024-02-27 17:01:54 +0530

    changes

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer-parent'>
+        <div className="col-12 footer-main-div">
+            <h4><b>C.R.I FLUID SYSTEMS PRODUCTS CATER TO DIVER SEGMENTS</b></h4>
+            <h6 className='footer-content'> 
+                <div>
+                    CHEMICALS & PROCESS <span>| </span>
+                    POWER <span>| </span> 
+                    WATER & WASTE WATER <span>| </span> 
+                    OILS & GAS <span>| </span>
+                    PHARMA <span>| </span>
+                    SUGARS & DISTILLERIES <span>| </span>
+                    PAPER & PULP <span>| </span>
+                    MARINE & DEFENCE <span>| </span>
+                    METAL & MINING <span>| </span>
+                    FOOD & BEVERAGE <span>| </span>
+                </div>
+                <div>
+                    PETRCHEMICAL & REFINERIES <span>| </span>
+                    SOLAR <span>| </span>
+                    BUILDING <span>| </span>
+                    HVAC <span>| </span>
+                    FIRE FIGHTING <span>| </span>
+                    AGRICULTURE & RESIDENTAL
+                </div>
+            </h6>
+        </div>
+        <div></div>
+        <div className="col-12 red-div">
+            <div>
+                <h5>Toll free <b>1800 200 1234</b></h5>
+            </div>
+            <div>
+                <h5><a href="www.facebook.com/cripumps">www.facebook.com/cripumps</a></h5>
+            </div>
+            <div>
+                <h5><a href="www.crigroups.com">www.crigroups.com</a></h5>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Footer
---
+++ src/Components/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col-lg-6 col-md-12 col-sm-12">
+                <h1>Hey there</h1>
+                <h1>Hey there</h1>
+                <h1>Hey there</h1>
+                <h1>Hey there</h1>
+                <h1>Hey there</h1>
+                <h1>Hey there</h1>
+                <h1>Hey there</h1>
+                <h1>Hey there</h1>
+                <h1>Hey there</h1>
+                <h1>Hey there</h1>
+                <h1>Hey there</h1>
+                <h1>Hey there</h1>
+                <h1>Hey there</h1>
+                <h1>Hey there</h1>
+            </div>
+            <div className="col-lg-6 col-md-12 col-sm-12">
+                <h1>Hey There</h1>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
