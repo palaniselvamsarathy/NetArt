c6c8d5c82b1276fb0a7822624d101c4de46a2f93
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2024-02-28 11:52:38 +0530

    changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -31,11 +31,18 @@ const Footer = () => {
         <div></div>
         <div className="col-12 red-div">
             <div className='img-div'>
-                <img src="./images/tf2.jpg" alt="TollFree" />
+                {/* <img src="./images/tf2.jpg" alt="TollFree" /> */}
+                {/* <box-icon name='phone' color='white' ></box-icon>
+                 */}
+                <i class='bx bx-phone'></i>
                 <h5>Toll free <b>1800 200 1234</b></h5>
             </div>
             <div className='img-div'>
-                <img src="./images/fb.png" alt="Facebook" />
+                {/* <img src="./images/fb.png" alt="Facebook" /> */}
+                <i class='bx bxl-facebook-circle' ></i>
+                {/* <box-icon name='facebook' type='logo' color='whi' ></box-icon>
+                 */}
+                {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12.001 2.002c-5.522 0-9.999 4.477-9.999 9.999 0 4.99 3.656 9.126 8.437 9.879v-6.988h-2.54v-2.891h2.54V9.798c0-2.508 1.493-3.891 3.776-3.891 1.094 0 2.24.195 2.24.195v2.459h-1.264c-1.24 0-1.628.772-1.628 1.563v1.875h2.771l-.443 2.891h-2.328v6.988C18.344 21.129 22 16.992 22 12.001c0-5.522-4.477-9.999-9.999-9.999z"></path></svg> */}
                 <h5><a href="www.facebook.com/cripumps">www.facebook.com/cripumps</a></h5>
             </div>
             <div className='img-div'>
